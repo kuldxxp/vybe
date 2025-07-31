6bcd4ebd871afa185ee6d4b16ec4eed087f41c36
kuldxxp <kuldxxp@kuldxxp.localdomain>
2025-07-31 13:05:10 +0530

    trpc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
-const Page = () => {
+import {Suspense} from "react";
+
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+
+import { getQueryClient, trpc } from "@/trpc/server";
+
+import { Client } from "./client";
+
+const Page = async () => {
+  const queryClient = getQueryClient();
+
+  void queryClient.prefetchQuery(trpc.createAI.queryOptions({ text: "Kuldeep PREFETCH" }));
+
   return (
     <div>
-      Hello World!      
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <Suspense fallback={ <p>Loading...</p> }>
+          <Client />
+        </Suspense>
+      </HydrationBoundary>
     </div>
   );
 }
