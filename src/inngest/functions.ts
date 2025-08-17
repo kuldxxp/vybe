512dbd21d4682f4c499799267d26ed507a16b770
Kuldeepsinh <kuldxxp@gmail.com>
2025-08-18 02:09:42 +0530

    aijobs-gemini-test

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,18 +1,52 @@
+// import { openai, createAgent } from "@inngest/agent-kit";
+import { Agent, createAgent, gemini } from "@inngest/agent-kit";
+
 import { inngest } from "./client";
+
+// export const helloWorld = inngest.createFunction(
+//   { id: "hello-world" },
+//   { event: "test/hello.world" },
+//   async ({ event }) => {
+//     //imaginary transcript step
+//     // await step.sleep("wait-a-moment", "5s");
+
+//     const codeAgent = createAgent({
+//       name: "code-agent",
+//       system: "You are an expert next.js developer. You write readable, maintainable code. You write simple Next.js & React snippets.",
+//       model: openai({ model: "gpt-4o" }),
+//     });
+
+//     const { output } = await codeAgent.run(
+//       `Write the following snippet: ${event.data.value}`,
+//     );
+//     console.log(output);
+
+
+//     // return { message: `Hello ${event.data.value}!` };
+//     return { output };
+//   },
+// );
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
-    //imaginary download step
-    await step.sleep("wait-a-moment", "30s");
+  async ({ event }) => {
+    const codeAgent = createAgent({
+      name: "code-agent",
+      system: "You are an expert next.js developer. You write readable, maintainable code. You write simple Next.js & React snippets.",
+      model: gemini({
+        model: "gemini-2.5-pro",
+        // apiKey: process.env.GOOGLE_API_KEY, // optional if already set in env
+        // baseUrl: "https://generativelanguage.googleapis.com/v1/", // default
+      }),
+    });
 
-    //imaginary transcript step
-    await step.sleep("wait-a-moment", "10s");
+    const { output } = await codeAgent.run(
+      `Write the following snippet: ${event.data.value}`,
+    );
 
-    //imaginary summary step
-    await step.sleep("wait-a-moment", "5s");
+    console.log(output);
 
-    return { message: `Hello ${event.data.email}!` };
+    return { output };
   },
 );
